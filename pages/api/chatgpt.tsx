84699b95a6c5cfcdb1036fd1fe7ff5e67036438b
Antônio <82171302+antonionetodeveloper@users.noreply.github.com>
2023-02-13 23:02:40 -0300

    Update chatgpt.tsx

--- pages/api/chatgpt.tsx
+++ pages/api/chatgpt.tsx
@@ -35,7 +35,7 @@ export default async function handler(
 			model: "text-davinci-003",
 			prompt: generatePrompt(question),
 			temperature: 0.6,
-			max_tokens: 250,
+			max_tokens: 150,
 			top_p: 1,
 			frequency_penalty: 0.0,
 			presence_penalty: 0.0,
